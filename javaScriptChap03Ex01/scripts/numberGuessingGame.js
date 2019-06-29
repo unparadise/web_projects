26e929bc09357a1fd7bad4bb6d7d7792ea7dfa04
unparadise <unparadise@users.noreply.github.com>
2019-06-29 05:35:21 -0700

    Add files via upload

---
+++ javaScriptChap03Ex01/scripts/numberGuessingGame.js
@@ -0,0 +1,42 @@
+let randomNumber = Math.floor(Math.random() * 100) +1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    if (guessCount == 1) {
+        guesses.textCount = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You git it right!';
+        lastResult.getElementsByClassName.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.getElementsByClassName.backgroundColor = 'red';
+        if(userGuess < randomNumber) {
+            lowOrHi.textContent = 'Last guess was too low!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Last guess was too high!';
+        }
+    }
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
