c662cac4c55f4684b4956bef122611fdad6fe2e6
Frank Chen <50078611+liangchen07@users.noreply.github.com>
2020-04-13 09:05:20 -0700

    added space

---
+++ Intro_to_Computation_02.04/main.py
@@ -0,0 +1,8 @@
+# This is the finger exercise in Chapter 2.4 (Iteration)
+
+numXs = int(input('How many times should I print the letter X? '))
+toPrint = ''
+while(numXs > 0):
+    toPrint += 'X'
+    numXs -= 1
+print(toPrint)
