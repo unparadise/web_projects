4bbb0d68bb53f9605c91cb795d1b619f852daaa5
Frank Chen <50078611+liangchen07@users.noreply.github.com>
2020-04-14 08:23:29 -0700

    Added finger exercise 02.05

---
+++ Intro_to_Computation_02.05/main.py
@@ -0,0 +1,29 @@
+# This is the finger exercise of 2.5 in book
+# Introduction to Computation and Programming Using Python
+# Write a program that examines ten variables and prints the 
+# largest odd number among them. If there is no odd numbers,
+# let the users know.
+
+# Ask the user to enter 3 integers
+allNumbersStr = input('Please enter 10 integers separated by comma: ')
+allNumbersStrList = allNumbersStr.split(',')
+print(allNumbersStrList)
+
+allNumbersInt = []
+
+oddNumbers = []
+
+i = 0
+while i < len(allNumbersStrList):
+    # Add the number to the oddNumber list if it is an odd number
+    if int(allNumbersStrList[i]) % 2 != 0:
+        oddNumbers.append(int(allNumbersStrList[i]))
+    i += 1
+
+# If there is no odd number then print the message to inform the users
+if len(oddNumbers) == 0:
+    print('All three numbers are even numbers')
+else:
+    oddNumbers.sort() # Sort the odd numbers in the list in the assending order
+    print('-----------------------------------')
+    print(oddNumbers[-1], 'is the largest odd number')
