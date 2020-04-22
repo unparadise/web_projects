1417df9eae66760832d83dc983f0311ec11933b6
unparadise <unparadise@users.noreply.github.com>
2020-04-21 22:17:31 -0700

    Added the 03.01 folder and main.py file

---
+++ Intro_to_Computation_03.01/main.py
@@ -0,0 +1,10 @@
+# This is the finger exercise of 3.1 in book
+# Introduction to Computation and Programming Using Python
+# Write a program that asks the user to enter an integer and
+# prints two integers, root and pwr, such that 0 < pwr < 6
+# and root**pwr is equal to the integer entered by the user.
+# If no such pair of integers exists, it should print a
+# message to that effect.
+
+number = int(input('Please enter an integer: '))
+print(number)
