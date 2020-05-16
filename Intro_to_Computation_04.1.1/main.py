3ac03493a3860b7b5d77b2e4a5e10c3c4793b8fa
Liang Chen <lianchen16@hotmail.com>
2020-05-15 23:21:36 -0700

    Added finger exercise 04.1.1

---
+++ Intro_to_Computation_04.1.1/main.py
@@ -0,0 +1,33 @@
+# This is the finger exercise in Chapter 4.1.1
+# Write a function isIn that accepts two strings
+# as arguments and returns True if either string
+# occurs in the other, and False otherwise.
+
+def isIn(stringOne, stringTwo):
+	if len(stringOne) > len(stringTwo):
+		# print('String one is longer than string two')
+		if stringOne.find(stringTwo) == -1:
+			return False
+		else:
+			print('String two is in string one')
+			return True
+	elif len(stringOne) < len(stringTwo):
+		# print('String one is shorter than string two')
+		if stringTwo.find(stringOne) == -1:
+			return False
+		else:
+			print('String one is in strnig two')
+			return True
+	else:
+		# print('String one and string two are same length')
+		if stringOne != stringTwo:
+			return False
+		else:
+			print('String one and string two are the same')
+			return True
+
+strOne = input('Please enter the first string: ')
+strTwo = input('Please enter the second string: ')
+
+print('--------------------------------')
+isIn(strOne, strTwo)
