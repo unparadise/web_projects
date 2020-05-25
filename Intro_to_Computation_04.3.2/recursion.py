26be865fab8f71a600472898cce205270f92bbda
Liang Chen <lianchen16@hotmail.com>
2020-05-25 08:04:33 -0700

    Added the recursive version of the palindrome function

--- Intro_to_Computation_04.3.2/recursion.py
+++ Intro_to_Computation_04.3.2/recursion.py
@@ -1,3 +1,33 @@
 # This program evaluate a string entered by the user
 # to see whether it is a palindrome using recursion
 
+def toChars(s):
+    s = s.lower()
+    letters = ''
+    for c in s:
+        if c in 'abcdefghijklmnopqrstuvwxyz':
+            letters = letters + c
+    
+    return letters
+
+
+def isPal(s):
+
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and isPal(s[1: -1])
+
+
+def isPalindrome(s):
+    """Decide whether a string is a palindrome"""
+
+    return isPal(toChars(s))
+
+
+userString = input('Please enter a string: ')
+
+if isPalindrome(userString):
+    print('The string you entered is a palindrome')
+else:
+    print('The string you entered is not a palindrome')
