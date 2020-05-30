e7542b7cbcbfcac1d19cc24fa190215b8fad7c70
unparadise <unparadise@users.noreply.github.com>
2020-05-30 14:02:55 -0700

    Changed the exchange rate implementation to use Dictionary

--- Exchange_Rate_Monitor/main.py
+++ Exchange_Rate_Monitor/main.py
@@ -2,8 +2,8 @@ import datetime
 import csv
 from forex_python.converter import CurrencyRates
 
+"""
 class ExchangeRate:
-    """Base class for forex rate"""
 
     fm = None
     to = None
@@ -27,3 +27,25 @@ class ExchangeRate:
 
 usdToCny = ExchangeRate('USD', 'CNY')
 usdToCny.printRate()
+"""
+
+exchangeRates = []
+todaysRate = {'Date': None, 'Rate': 0, 'From': None, 'To': None}
+
+def getDate():
+    x = datetime.datetime.now()
+    d = str(x.year) + '.' + str(x.month) + '.' + str(x.day)
+
+    return d
+
+def getRate(date, fromCurrency, toCurrency):
+    c = CurrencyRates()
+    rate = c.get_rate(fromCurrency, toCurrency)
+    rate = format(rate, '.2f')
+
+    return {'Date': date, 'Rate': rate, 'From': fromCurrency, 'To': toCurrency}
+
+date = getDate()
+todaysRate = getRate(date, 'USD', 'CNY')
+exchangeRates.append(todaysRate)
+print(exchangeRates)
