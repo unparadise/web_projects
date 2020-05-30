5123efaa8f628d1c71bf91cd32d04daf1de0264d
unparadise <unparadise@users.noreply.github.com>
2020-05-30 14:03:22 -0700

    Removed the class code

--- Exchange_Rate_Monitor/main.py
+++ Exchange_Rate_Monitor/main.py
@@ -2,33 +2,6 @@ import datetime
 import csv
 from forex_python.converter import CurrencyRates
 
-"""
-class ExchangeRate:
-
-    fm = None
-    to = None
-    date = None
-    rate = 0
-
-    def __init__(self, fromCurrency, toCurrency):
-        self.fm = fromCurrency
-        self.to = toCurrency
-
-        x = datetime.datetime.now()
-        self.date = str(x.year) + '.' + str(x.month) + '.' + str(x.day)
-        
-        c = CurrencyRates()
-        self.rate = c.get_rate(self.fm, self.to)
-        self.rate = format(self.rate, '.2f')
-    
-    def printRate(self):
-        print(self.date, ':', self.rate)
-
-
-usdToCny = ExchangeRate('USD', 'CNY')
-usdToCny.printRate()
-"""
-
 exchangeRates = []
 todaysRate = {'Date': None, 'Rate': 0, 'From': None, 'To': None}
 
