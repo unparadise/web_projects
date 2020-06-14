c4d59099be609b2357706f716bdbd05f26404892
Liang Chen <lianchen16@hotmail.com>
2020-06-14 00:24:56 -0700

    Added the history_rates.py file

---
+++ Exchange_Rate_Monitor/history_rates.py
@@ -0,0 +1,25 @@
+# This program retrieves historical exchange rate
+# from USD to RMB using https://ratesapi.io.
+
+import datetime
+from forex_python.converter import CurrencyRates
+
+def createDates():
+	"""This function puts a list of dates and returns
+	these dates in a tuple"""
+
+
+
+def getRates(dates, fromCurrency, toCurrency):
+	"""This function retrieves the historical exchange
+	rates from USD to RMB using the list of dates passed
+	to it. It returns a list of dictionaries that contains
+	the Date, Exhange Rate, From currency, and currency
+	key"""
+
+
+def saveRates(data, filePath):
+	"""This function writes the date to the file specified
+	by the parameters"""
+
+	
