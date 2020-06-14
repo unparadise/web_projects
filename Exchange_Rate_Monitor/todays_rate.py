5813d1a8358914466676ff4afe8e28eb9052f2e0
Liang Chen <lianchen16@gmail.com>
2020-06-14 09:15:07 -0700

    Refactored getRate, getDate, sendEmail, and saveRates into separate modules

--- Exchange_Rate_Monitor/todays_rate.py
+++ Exchange_Rate_Monitor/todays_rate.py
@@ -1,80 +1,14 @@
 import datetime
 import csv
-from forex_python.converter import CurrencyRates
-import smtplib
+from get_date import getDate
+from save_rates import saveRates
+from get_rate import getRate
+from send_email import sendEmail
+
 
 exchangeRates = []
 todaysRate = {'Date': None, 'Rate': 0, 'From': None, 'To': None}
-
-def getDate():
-    x = datetime.datetime.now()
-    d = str(x.year) + '.' + str(x.month) + '.' + str(x.day)
-
-    # print(x.date())
-    return x
-
-def getRate(date, fromCurrency, toCurrency):
-    """Retrieve the exchange rate from https://ratesapi.io.
-    The function returns a dictionary object with date, rate,
-    from, and to keys
-    """
-
-    c = CurrencyRates()
-    rate = c.get_rate(fromCurrency, toCurrency, date)
-    rate = format(rate, '.2f')
-
-    return {'Date': str(date.date()), 'Rate': rate, 'From': fromCurrency, 'To': toCurrency}
-
-def sendemail(date, exRate, fmCurrency, toCurrency, receiverEmails): 
-    """Send email to a specific address"""
-
-    gmailSenderEmail = 'lianchen16@gmail.com'
-    gmailPassword = 'iwayfbrismavabfy'
-      
-    emailSubject = 'Exchange rate alert!' 
-
-    # creates SMTP session  
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-      
-    # start TLS for security  
-    server.starttls()  
-      
-    # Authentication  
-    server.starttls()  
-      
-    server.login(gmailSenderEmail, gmailPassword)  
-      
-    # message to be sent  
-    headers = "\r\n".join(["from: " + gmailSenderEmail, 
-                            "subject: " + emailSubject, 
-                            "to: " + receiverEmails, 
-                            "mime-version: 1.0", 
-                            "content-type: text/html"]) 
-      
-    message = headers + 'The exchange rate from ' + fmCurrency + ' to ' + toCurrency + ' on ' + date + ' is ' + exRate
-    
-    try:
-        server.sendmail(gmailSenderEmail, receiverEmails.split(','), message)
-        print('Email sent')
-    except SMTPException:
-        print('Error: unable to send email')
-    server.quit()
-
-## TODO: Update the code to only write the new data
-## TODO: Move the function to a separate file @tomorrow
-def saveRates(rates, file, csvColumns):
-    try:
-        with open(file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile,fieldnames=csvColumns)
-            writer.writeheader()
-            for data in rates:
-                writer.writerow(data)
-    except IOError:
-        print('Can\'t write to file. I/O error.')
-
-
-# This is the beginning of the main program
+alertRate = 7.0
 
 date = getDate()
 #csvFile = './web_projects/Exchange_Rate_Monitor/exchange_rates.csv'
@@ -90,12 +24,17 @@ except IOError:
     
 # If today is not in the exchangeRates list then add today's date,
 # rate, from, and to data as a dictionary item to the list
+## TODO: move the code that decides whether there is new data to the save_rates.py file
 if len(exchangeRates) == 0 or exchangeRates[len(exchangeRates)-1]['Date'] != str(date.date()):
-        todaysRate = getRate(date, 'USD', 'CNY')
-        exchangeRates.append(todaysRate)
-        if float(todaysRate['Rate']) < 7.0:
-            sendemail(todaysRate['Date'], todaysRate['Rate'], todaysRate['From'], todaysRate['To'], 'lianchen16@gmail.com')
+    todaysRate = getRate(date, 'USD', 'CNY')
+    exchangeRates.append(todaysRate)
+    if float(todaysRate['Rate']) < alertRate:
+        sendEmail(todaysRate['Date'], todaysRate['Rate'], todaysRate['From'], todaysRate['To'], 'lianchen16@gmail.com')
 
-# Write the exchangeRates list to the exchange_rates.csv file
-saveRates(exchangeRates, csvFile, csvColumns)
-print(exchangeRates)
+    # Write the exchangeRates list to the exchange_rates.csv file
+    saveRates(exchangeRates, csvFile, csvColumns)
+    print(exchangeRates)
+
+else:
+    print('Today\'s rate is already retrieved.')
+    print(exchangeRates[len(exchangeRates)-1])
