4805acf654d6b1fae0554641e927707d87112a4b
Liang Chen <lianchen16@gmail.com>
2020-06-18 23:15:00 -0700

    Changed the historical date range

--- Exchange_Rate_Monitor/history_rates.py
+++ Exchange_Rate_Monitor/history_rates.py
@@ -6,7 +6,7 @@ from forex_python.converter import CurrencyRates
 from get_rate import getRate
 from save_rates import saveRates
 	
-dates = createDates('2020-06-10', '2020-06-13')
+dates = createDates('2008-01-01', '2020-06-13')
 
 rates = []
 for date in dates:
