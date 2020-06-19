a3c7e31ca5f33facccc7761607c371ddc8605f5b
Liang Chen <lianchen16@gmail.com>
2020-06-18 22:44:53 -0700

    Refactored the createDate() function and separated it into another module file

--- Exchange_Rate_Monitor/history_rates.py
+++ Exchange_Rate_Monitor/history_rates.py
@@ -1,32 +1,16 @@
 # This program retrieves historical exchange rateT
 # from USD to RMB using https://ratesapi.io.
-# TODO: Move the createDates() function to a separate module
 
-import datetime
+from create_dates import createDates
 from forex_python.converter import CurrencyRates
 from get_rate import getRate
 from save_rates import saveRates
-
-def createDates(startDate, endDate):
-	"""This function puts a list of dates and returns
-	these dates in a tuple"""
-	dates = []
-
-	start = datetime.datetime.strptime(startDate, '%Y-%m-%d')
-	end = datetime.datetime.strptime(endDate, '%Y-%m-%d')
-	step = datetime.timedelta(days=1)
-	while start <= end:
-	    # print(start.date())
-	    dates.append(start.date())
-	    start += step
-
-	return dates
 	
-dates = createDates('2010-05-03', '2020-06-13')
+dates = createDates('2020-06-10', '2020-06-13')
 
 rates = []
 for date in dates:
-	rates.append({'Date':date, 'Rate': getRate(date, 'USD', 'CNY'), 'From': 'USD', 'To': 'CNY'})
+	rates.append(getRate(date, 'USD', 'CNY'))
 	# print(rates)
 
 csvFile = './history_rates.csv'
