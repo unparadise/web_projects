23e21ff724242bd50fa6441c2891427b04e24ae1
Liang Chen <lianchen16@gmail.com>
2020-07-04 09:09:16 -0700

    Added code to handle date arguments

--- Exchange_Rate_Monitor/history_rates.py
+++ Exchange_Rate_Monitor/history_rates.py
@@ -5,15 +5,22 @@ from create_dates import createDates
 from forex_python.converter import CurrencyRates
 from get_rate import getRate
 from save_rates import saveRates
-	
-dates = createDates('2008-01-01', '2020-06-13')
+import sys
+
+try:
+	startDate = sys.argv[1]
+	endDate = sys.argv[2]
+except:
+	print('Try history_rates.py [start date] [end date]')
+
+dates = createDates(startDate, endDate)
 
 rates = []
 for date in dates:
 	rates.append(getRate(date, 'USD', 'CNY'))
 	# print(rates)
 
-csvFile = './history_rates.csv'
+csvFile = './history_rates_test.csv'
 csvColumns = ['Date', 'Rate', 'From', 'To']
 
 saveRates(rates, csvFile, csvColumns)
