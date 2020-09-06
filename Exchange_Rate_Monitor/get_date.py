406b7f0cb62a166d453073f791423fe62a8904ba
Liang Chen <liangchen@Liangs-MacBook-Pro.local>
2020-09-05 23:12:55 -0700

    Removed debug code in get_date module

--- Exchange_Rate_Monitor/get_date.py
+++ Exchange_Rate_Monitor/get_date.py
@@ -4,7 +4,5 @@ import datetime
 def getDate():
     currentDate = datetime.date.today()
     #print(currentDate)
-    #x = datetime.now().strftime('%Y-%m-%d')
-    #x = datetime.strptime(x, '%Y-%m-%d')
 
     return currentDate
