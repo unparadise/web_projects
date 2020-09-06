ec7695898e9813aa9cb919e5a3975c7f105206dd
Liang Chen <liangchen@Liangs-MacBook-Pro.local>
2020-09-05 17:34:42 -0700

    Update the exchange_rate file in the folder

--- Exchange_Rate_Monitor/get_date.py
+++ Exchange_Rate_Monitor/get_date.py
@@ -3,7 +3,7 @@ import datetime
 
 def getDate():
     currentDate = datetime.date.today()
-    print(currentDate)
+    #print(currentDate)
     #x = datetime.now().strftime('%Y-%m-%d')
     #x = datetime.strptime(x, '%Y-%m-%d')
 
