94b0c46e80821341340df5a81de9ed14fbf9d789
lianchen16@gmail.com <carman.8>
2020-10-27 07:51:16 -0700

    Changed the code to use Django form function

--- CS50/Django/lecture3/tasks/views.py
+++ CS50/Django/lecture3/tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Tasks")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -9,4 +14,6 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
