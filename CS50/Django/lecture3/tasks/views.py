c4910a5d56b5883de13dfa670e2d331d37891d6d
lianchen16@gmail.com <carman.8>
2020-10-28 17:03:39 -0700

    Added code to redirect the home page after a task is added

--- CS50/Django/lecture3/tasks/views.py
+++ CS50/Django/lecture3/tasks/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Tasks")
@@ -19,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
