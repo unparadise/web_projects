5ff1c545a7d7a551a1edb02e2038361b167d5a31
Liang Chen <liangchen@Liangs-MacBook-Pro.local>
2020-11-01 08:25:30 -0800

    Completed the database migration steps

--- CS50/SQL/airline/airline/urls.py
+++ CS50/SQL/airline/airline/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flights/", include("flghts.urls"))
+    path("flights/", include("flights.urls"))
 ]
