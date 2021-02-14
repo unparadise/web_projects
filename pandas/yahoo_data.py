eb82163fce3a2b69f0be16b3db10194bed1983b1
unparadise <unparadise@users.noreply.github.com>
2021-02-14 08:20:54 -0800

    Add date to saved sqlit3 database

--- pandas/yahoo_data.py
+++ pandas/yahoo_data.py
@@ -9,9 +9,10 @@
 # Yahoo Finance Library other useful methods
 # get_market_cap()
 
-
 from yahoofinancials import YahooFinancials
 import pandas as pd
+import sqlite3 as db
+from datetime import date
 
 companiesData = pd.read_csv('./S&P_500_tickers.csv')
 #print(type(companiesData))
@@ -22,7 +23,7 @@ companyTickers = companiesData['Symbol'].tolist()
 
 pe_ratios = []
 market_caps = []
-
+dividend_rate = []
 
 # For each company in the S&P 500 list, extract its PE ratio and market cap and add each of them to its respective list
 for ticker in companyTickers:
@@ -30,9 +31,17 @@ for ticker in companyTickers:
     print('Downloading data for ' + ticker + '...')
     pe_ratios.append(yahoo_financials.get_pe_ratio())
     market_caps.append(yahoo_financials.get_market_cap())
-
+    dividend_rate.append(yahoo_financials.get_market_cap())
+    
 companiesData['P/E'] = pe_ratios
 companiesData['Market Cap'] = market_caps
+companiesData['Dividend Rate'] = dividend_rate
 
 #print(companiesData)
-companiesData.to_csv("./S&P_500.csv", index=False)
+#companiesData.to_csv("./S&P_500.csv", index=False)
+date = str(date.today())
+conn = db.connect('SP500'+ date + '.db')
+companiesData.to_sql(name='SP500', con=conn)
+
+# Another useful python library is the Fundamental Analysis library
+# https://github.com/JerBouma/FundamentalAnalysis
