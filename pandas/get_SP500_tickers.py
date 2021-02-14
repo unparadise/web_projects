bc13f19cd973fd95d78bab02c806e3d7a5429e64
Liang Chen <lianchen16@gmail.com>
2021-02-14 14:12:14 -0800

    Added get_SP500 file

--- pandas/get_SP500_tickers.py
+++ pandas/get_SP500_tickers.py
@@ -1,19 +1,24 @@
 import bs4 as bs
-import pickle
 import requests
+import csv
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'html.parser')
     table = soup.find('table', {'class': 'wikitable sortable'})
+    header = ['Ticker']
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
-        tickers.append(ticker)
+        ticker = ticker[0:-1]
+        tickers.append([ticker])
 
-    with open("sp500tickers.pickle", "wb") as f:
-        pickle.dump(tickers, f)
-
+    print(tickers)
+    with open('sp500tickers.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(tickers)
+        
     return tickers
 
 save_sp500_tickers()
